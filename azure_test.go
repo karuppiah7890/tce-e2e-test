4afdb5ad720d68ddbb5a3fa369a91ff4697fc9d6
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-04-07 16:50:41 +0530

    add check for required env vars and fail when they are not present
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- azure_test.go
+++ azure_test.go
@@ -1,6 +1,11 @@
 package e2e
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+)
 
 func TestAzureManagementAndWorkloadCluster(t *testing.T) {
 	// Ensure TCE/TF is installed - check TCE installation or install it if not present. Or do it prior to the test run.
@@ -13,6 +18,18 @@ func TestAzureManagementAndWorkloadCluster(t *testing.T) {
 	// If the required env vars are not present, throw error and give information about how to go about fixing the error - by
 	// getting the secrets from appropriate place with the help of docs, maybe link to appropriate Azure or TCE or TKG docs for this.
 	// Ensure that the secrets are NEVER logged into the console
+	requiredEnvVars := []string{
+		"AZURE_CLIENT_ID",
+		"AZURE_CLIENT_SECRET",
+		"AZURE_SSH_PUBLIC_KEY_B64",
+		"AZURE_SUBSCRIPTION_ID",
+		"AZURE_TENANT_ID",
+	}
+	errs := checkRequiredEnvVars(requiredEnvVars)
+
+	if len(errs) != 0 {
+		log.Fatalf("Errors: %v\n", errs)
+	}
 
 	// Have different log levels - none/minimal, error, info, debug etc, so that we can accordingly use those in the E2E test
 
@@ -23,4 +40,18 @@ func TestAzureManagementAndWorkloadCluster(t *testing.T) {
 	// Hard code the value of the inputs required / needed / necessary for accepting Azure VM image license / terms
 
 	// Accept the VM image license / terms
+}
+
+func checkRequiredEnvVars(requiredEnvVars []string) []error {
+	errors := make([]error, 0, len(requiredEnvVars))
+	for _, envVar := range requiredEnvVars {
+		// TODO: Do we also error out when the env var value is defined but empty??
+		_, isDefined := os.LookupEnv(envVar)
+
+		if !isDefined {
+			errors = append(errors, fmt.Errorf("Environment variable `%s` is required but not defined", envVar))
+		}
+	}
+
+	return errors
 }
