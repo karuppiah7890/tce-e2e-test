9f469155d7339cace1fba182c15e35739443dcb8
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-04-15 15:36:17 +0530

    wip
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

---
+++ testutils/docker/docker_test.go
@@ -0,0 +1,11 @@
+package docker_test
+
+import (
+	"testing"
+
+	"github.com/karuppiah7890/tce-e2e-test/testutils/log"
+)
+
+func TestContainerDelete(t *testing.T) {
+	log.InitLogger("TestContainerDelete")
+}
