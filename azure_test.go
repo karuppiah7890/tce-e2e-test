cfe5ea77962c589cdbcc240dd3158f45a47d2e34
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-04-28 17:29:21 +0530

    update todos
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- azure_test.go
+++ azure_test.go
@@ -752,7 +752,7 @@ func deleteWorkloadCluster(workloadClusterName string) error {
 }
 
 // TODO: We can get this struct from Tanzu Framework pkg/library too
-// from `ClusterInfo` within `github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/client` pkg in 7275f31196fd9d4f60946c9cbb5429baf7136f3b
+// from `ClusterInfo` within `https://pkg.go.dev/github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/client` pkg in 7275f31196fd9d4f60946c9cbb5429baf7136f3b
 // version of Tanzu Framework which is after v0.20.0
 // TODO: Move this to a common util
 type WorkloadCluster struct {
