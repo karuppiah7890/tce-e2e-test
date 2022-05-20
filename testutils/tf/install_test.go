e318c398a59b38e5e19a1c2b561dad4034d548cc
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-05-20 13:32:12 +0530

    check tanzu version as part of tce and tf install test
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- testutils/tf/install_test.go
+++ testutils/tf/install_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/karuppiah7890/tce-e2e-test/testutils/log"
+	"github.com/karuppiah7890/tce-e2e-test/testutils/tanzu"
 	"github.com/karuppiah7890/tce-e2e-test/testutils/tf"
 )
 
@@ -13,5 +14,10 @@ func TestTfLegacyInstall(t *testing.T) {
 	err := tf.LegacyInstall(version)
 	if err != nil {
 		log.Fatalf("error occurred while installing TC version %s: %v", version, err)
+	}
+
+	err = tanzu.PrintTanzuVersion()
+	if err != nil {
+		log.Fatalf("expected no error but error occurred while printing tanzu CLI version: %v", err)
 	}
 }
