96e45d970a25793bd9a3a27dfe94951d30b82e31
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-05-23 16:18:23 +0530

    use binaries from the Internet to run tests for signature verification
    this way anyone can run the tests and we dont have to embed any sample binaries
    in the git repo unnecessarily
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- testutils/codesign/verify_test.go
+++ testutils/codesign/verify_test.go
@@ -1,32 +1,52 @@
 package codesign_test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/karuppiah7890/tce-e2e-test/testutils/codesign"
+	"github.com/karuppiah7890/tce-e2e-test/testutils/download"
+	"github.com/karuppiah7890/tce-e2e-test/testutils/extract"
 	"github.com/karuppiah7890/tce-e2e-test/testutils/log"
 )
 
 func TestVerify(t *testing.T) {
+
 	log.InitLogger("verify-binary")
 
-	t.Run("binary with valid signature", func(t *testing.T) {
-		// TODO: Use a binary path that can work on all machines
-		binPath := "/Users/karuppiahn/Downloads/tce-darwin-amd64-v0.11.0/tanzu"
-		err := codesign.Verify(binPath)
+	testDir, err := os.MkdirTemp(os.TempDir(), "verify test")
+	if err != nil {
+		log.Fatalf("expected no error while creating temporary directory for test, but got error: %v", err)
+	}
+
+	t.Run("binary with valid signature at a path containing space in file path", func(t *testing.T) {
+		artifactPath := filepath.Join(testDir, "tanzu-cli-darwin-amd64.tar.gz")
+		err = download.DownloadFileFromUrl("https://github.com/vmware-tanzu/tanzu-framework/releases/download/v0.21.0/tanzu-cli-darwin-amd64.tar.gz", artifactPath)
+		if err != nil {
+			log.Fatalf("expected no error while downloading artifact, but got error: %v", err)
+		}
+		extract.Extract(artifactPath, testDir)
+		binPath := filepath.Join(testDir, "v0.21.0", "tanzu-core-darwin_amd64")
+		err = codesign.Verify(binPath)
 		if err != nil {
 			log.Fatalf("expected no error but got error: %v", err)
 		}
 	})
 
-	t.Run("binary with invalid signature", func(t *testing.T) {
-		// TODO: Use a binary path that can work on all machines
-		binPath := "/Users/karuppiahn/Downloads/tce-darwin-amd64-v0.12.0-dev.1/tanzu"
+	t.Run("binary with invalid signature at a path containing space in file path", func(t *testing.T) {
+		artifactPath := filepath.Join(testDir, "kubectl")
+		err = download.DownloadFileFromUrl("https://dl.k8s.io/v1.24.0/bin/darwin/amd64/kubectl", artifactPath)
+		if err != nil {
+			log.Fatalf("expected no error while downloading artifact, but got error: %v", err)
+		}
+		binPath := filepath.Join(testDir, "kubectl")
 		err := codesign.Verify(binPath)
 		if err == nil {
 			log.Fatalf("expected error but got no error")
 		}
 
-		// TODO: verify the error message content
+		// TODO: verify the error message content because the error should be because of signing issues and not due
+		// to any other error
 	})
 }
