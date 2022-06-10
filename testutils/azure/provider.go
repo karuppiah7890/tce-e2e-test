7b1a7a9edaf6f04a9fb89d5cc12c9fc28dd4f369
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-06-10 16:34:51 +0530

    fix azure provider Init method
    Previously the init didn't work as it used a copy of the provider to initialize credentials.
    All methods now use pointer receivers. This was caught by go-staticcheck linter with one of the
    errors saying "ineffective assignment to field Provider.testSecrets (SA4005)"
    
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- testutils/azure/provider.go
+++ testutils/azure/provider.go
@@ -14,16 +14,16 @@ type Provider struct {
 	testSecrets TestSecrets
 }
 
-func (provider Provider) CheckRequiredEnvVars() bool {
+func (provider *Provider) CheckRequiredEnvVars() bool {
 	CheckRequiredAzureEnvVars()
 	return true
 }
 
-func (provider Provider) Name() string {
+func (provider *Provider) Name() string {
 	return "azure"
 }
 
-func (provider Provider) Init() error {
+func (provider *Provider) Init() error {
 	provider.testSecrets = ExtractAzureTestSecretsFromEnvVars()
 
 	provider.cred = Login()
@@ -31,7 +31,7 @@ func (provider Provider) Init() error {
 	return nil
 }
 
-func (provider Provider) PreClusterCreationTasks(clusterName string, clusterType utils.ClusterType) error {
+func (provider *Provider) PreClusterCreationTasks(clusterName string, clusterType utils.ClusterType) error {
 	azureMarketplaceImageInfoForCluster := GetAzureMarketplaceImageInfoForCluster(clusterName, clusterType)
 
 	// TODO: make the below function return an error and handle the error to log and exit?
@@ -40,7 +40,7 @@ func (provider Provider) PreClusterCreationTasks(clusterName string, clusterType
 	return nil
 }
 
-func (provider Provider) CleanupCluster(ctx context.Context, clusterName string) error {
+func (provider *Provider) CleanupCluster(ctx context.Context, clusterName string) error {
 	err := DeleteResourceGroup(context.TODO(), clusterName, provider.testSecrets.SubscriptionID, provider.cred)
 	if err != nil {
 		return fmt.Errorf("error while cleaning up azure resource group of the cluster which has all the cluster resources: %v", err)
@@ -49,4 +49,4 @@ func (provider Provider) CleanupCluster(ctx context.Context, clusterName string)
 }
 
 // TODO: Change name?
-var PROVIDER utils.Provider = Provider{}
+var PROVIDER utils.Provider = &Provider{}
