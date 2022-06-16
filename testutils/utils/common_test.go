d659deca1b8a718dd5a314f124541d65c2c6e7ee
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-06-16 17:59:55 +0530

    add test for RunProviderTest function for workload cluster creation failure scenario
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- testutils/utils/common_test.go
+++ testutils/utils/common_test.go
@@ -53,4 +53,59 @@ func TestRunProviderTest(t *testing.T) {
 		}
 	})
 
+	t.Run("when workload cluster creation fails it should collect diagnostics", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+
+		defer ctrl.Finish()
+
+		provider := mock_utils.NewMockProvider(ctrl)
+		r := mock_utils.NewMockClusterTestRunner(ctrl)
+
+		provider.EXPECT().Name().Return("mock-infra")
+
+		gomock.InOrder(
+			r.EXPECT().RunChecks(),
+
+			provider.EXPECT().CheckRequiredEnvVars(),
+
+			provider.EXPECT().Init(),
+
+			r.EXPECT().
+				GetRandomClusterNames().Return("test-mgmt", "test-wkld"),
+
+			provider.EXPECT().PreClusterCreationTasks("test-mgmt", utils.ManagementClusterType),
+
+			r.EXPECT().GetKubeContextForTanzuCluster("test-mgmt").Return("mock-context-1"),
+
+			r.EXPECT().GetKubeConfigPath().Return("mock-config-path", nil),
+
+			r.EXPECT().
+				RunCluster("test-mgmt", provider, utils.ManagementClusterType),
+
+			r.EXPECT().GetClusterKubeConfig("test-mgmt", provider, utils.ManagementClusterType),
+
+			r.EXPECT().PrintClusterInformation("mock-config-path", "mock-context-1"),
+
+			provider.EXPECT().PreClusterCreationTasks("test-wkld", utils.WorkloadClusterType),
+
+			r.EXPECT().GetKubeContextForTanzuCluster("test-wkld").Return("mock-context-2"),
+
+			r.EXPECT().
+				RunCluster("test-wkld", provider, utils.WorkloadClusterType).
+				Return(fmt.Errorf("some error in workload cluster creation")),
+
+			r.EXPECT().CollectManagementClusterAndWorkloadClusterDiagnostics("test-mgmt", "test-wkld", "mock-infra"),
+			provider.EXPECT().CleanupCluster(gomock.Any(), "test-mgmt"),
+			r.EXPECT().DeleteContext("mock-config-path", "mock-context-1"),
+			provider.EXPECT().CleanupCluster(gomock.Any(), "test-wkld"),
+			r.EXPECT().DeleteContext("mock-config-path", "mock-context-2"),
+		)
+
+		err := utils.RunProviderTest(provider, r)
+		expectedError := "error while running workload cluster: some error in workload cluster creation"
+		if err.Error() != expectedError {
+			t.Logf("expected error to be: %v. But got: %v", expectedError, err)
+			t.Fail()
+		}
+	})
 }
