2992d1f63c9c4f7ca2e3e6f7d2bacbd94917b4da
Aman Sharma <amansh@vmware.com>
2022-06-27 12:37:34 +0530

    supporting all the providers for package E2E test hook
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- packages_e2e_test.go
+++ packages_e2e_test.go
@@ -4,11 +4,14 @@ import (
 	"os"
 	"testing"
 
+	"github.com/karuppiah7890/tce-e2e-test/testutils/aws"
+	"github.com/karuppiah7890/tce-e2e-test/testutils/azure"
 	"github.com/karuppiah7890/tce-e2e-test/testutils/dockerprovider"
 	"github.com/karuppiah7890/tce-e2e-test/testutils/github"
 	"github.com/karuppiah7890/tce-e2e-test/testutils/log"
 	"github.com/karuppiah7890/tce-e2e-test/testutils/tce"
 	"github.com/karuppiah7890/tce-e2e-test/testutils/utils"
+	"github.com/karuppiah7890/tce-e2e-test/testutils/vsphere"
 )
 
 func TestCloneTCERepo(t *testing.T) {
@@ -17,12 +20,32 @@ func TestCloneTCERepo(t *testing.T) {
 		log.Errorf("Error while cloning TCE Repo: %v", err)
 	}
 
-	log.InitLogger("docker-mgmt-wkld-e2e")
-
 	packageDetails := tce.Package{}
 	packageDetails.Name = os.Getenv("PACKAGE_NAME")
 	packageDetails.Version = os.Getenv("PACKAGE_VERSION")
+	provider := os.Getenv("PROVIDER")
 
+	log.InitLogger(provider + "-mgmt-wkld-e2e")
 	r := utils.DefaultClusterTestRunner{}
-	utils.RunProviderTest(dockerprovider.PROVIDER, r, packageDetails)
+
+	if provider == "docker" {
+		// Running package E2E test on docker
+		err = utils.RunProviderTest(dockerprovider.PROVIDER, r, packageDetails)
+	} else if provider == "aws" {
+		// Running package E2E test on AWS
+		err = utils.RunProviderTest(aws.PROVIDER, r, packageDetails)
+	} else if provider == "azure" {
+		// Running package E2E test on Azure
+		err = utils.RunProviderTest(azure.PROVIDER, r, packageDetails)
+	} else if provider == "vsphere" {
+		// Running package E2E test on vSphere
+		utils.RunProviderTest(vsphere.PROVIDER, r, tce.Package{})
+	} else {
+		// Invalid provider in PROVIDER env variable
+		t.Errorf("Ivalid provider for package E2E Test")
+	}
+
+	if err != nil {
+		t.Errorf("Error while running package E2E test on %v: %v", provider, err)
+	}
 }
